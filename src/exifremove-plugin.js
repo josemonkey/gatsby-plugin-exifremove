30a26feb629b4f765bc693bcbfe58c764cbf730f
josemonkey <josemonkey@josemonkey.com>
2024-10-14 10:45:31 -0400

    Hardening code

--- src/exifremove-plugin.js
+++ src/exifremove-plugin.js
@@ -5,11 +5,14 @@ const exifRemove = require("exifremove");
 
 const { isObject, isBoolean, deepMerge } = require('./helpers');
 
+const pluginName = 'gatsby-plugin-exifremove';
+
 const readFileAsync = util.promisify(fs.readFile);
 
 const defaultOptions = {
     debug: false,
-    config: {
+    matchPattern: "**/*.{jpg,jpeg}",
+    exifremoveConfig: {
         keepMarker: false,
         verbose: false
     },
@@ -20,37 +23,52 @@ async function onPostBuild(args, pluginOptions = {}) {
 
     if (pluginOptions) {
         if (pluginOptions.debug && !isBoolean(pluginOptions.debug)) {
-            throw new Error('EXIF remove error, at plugin options `debug` value not type boolean false or true.');
+            throw new Error(`${pluginName} error reading options: 'debug' value not a valid boolean value.`);
         }
-        if (pluginOptions.config && !isObject(pluginOptions.config)) {
-            throw new Error('EXIF remove error, at plugin options `config` value not type object.');
+        if (pluginOptions.matchPattern && !isString(pluginOptions.matchPattern)) {
+            throw new Error(`${pluginName} error reading options: 'matchPattern' value not a string.`);
+        }
+        if (pluginOptions.exifremoveConfig && !isObject(pluginOptions.exifremoveConfig)) {
+            throw new Error(`${pluginName} error reading options: 'exifremoveConfig' value not an object.`);
         }
     }
     const options = deepMerge(defaultOptions, pluginOptions);
 
-    const pattern = 'public/**/*.{jpg,jpeg}';
+    console.info(`Removing EXIF data from JPEG images.`);
+
+    const pattern = `public/${options.matchPattern}`;
+
+    if (options.debug) {
+        console.log(`Looking for JPEG files with pattern '${pattern}'`);
+    }
     const files = await glob.glob(pattern, { nodir: true });
+    if (options.debug) {
+        console.log(`Files found: ${files.length}`);
+    }
+
+    if (options.debug) {
+        console.log(`Using exifremove options: ${JSON.stringify(options.exifremoveConfig)}`);
+    }
 
     const pluginStart = new Date().getTime();
-    const allFilesString = `Removing EXIF data from JPEG images in public directory; total files found: ${files.length}`;
-    console.info(options.debug ? `${allFilesString}:` : `${allFilesString}.`);
 
     const processedFiles = files.map(async (file) => {
 
+        if (options.debug) {
+            console.info(`Reading ${file}...`);
+        }
         const data = await readFileAsync(file);
+
         return new Promise((resolve, reject) => {
 
             console.info(`Processing ${file}...`);
 
             let scrubbedImage;
             try {
-                if (options.verbose) {
-                    console.log(`Image file length: ${data.length}`);
-                }
 
                 scrubbedImage = exifRemove.remove(data, {
-                    keepMarker: options.keepMarker,
-                    verbose: options.verbose
+                    keepMarker: options.exifremoveConfig.keepMarker,
+                    verbose: options.exifremoveConfig.verbose
                 });
 
 
@@ -60,13 +78,16 @@ async function onPostBuild(args, pluginOptions = {}) {
             }
 
             fs.writeFile(file, scrubbedImage, (err) => {
+                if (options.debug) {
+                    console.info(`Writing ${file}...`);
+                }
                 if (err) {
                     reject();
                     console.error(`EXIF removal error on write file:\n\n${err}`);
                 }
                 resolve();
             });
-            
+
 
         });
     });
